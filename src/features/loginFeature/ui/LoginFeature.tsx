17b1341c346e01601f6b966cb954ff3879ad21af
Ororura <hitmanlol890@gmail.com>
2024-09-04 15:35:50 +0300

    add registration page

--- src/features/loginFeature/ui/LoginFeature.tsx
+++ src/features/loginFeature/ui/LoginFeature.tsx
@@ -4,20 +4,19 @@ import { useForm } from "react-hook-form";
 import { useLogin } from "@/features/loginFeature/lib/hooks";
 
 import { LoginData } from "@/features/loginFeature/types";
+import s from "./LoginFeature.module.css";
 
 const LoginFeature: FC = () => {
   const { register, handleSubmit } = useForm<LoginData>();
   const { login } = useLogin();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(login)}>
-        <p>Авторизоваться</p>
-        <input type="text" placeholder="Логин" {...register("username")} />
-        <input type="text" placeholder="Пароль" {...register("password")} />
-        <button type="submit">Войти</button>
-      </form>
-    </div>
+    <form className={s.formContainer} onSubmit={handleSubmit(login)}>
+      <p>Авторизоваться</p>
+      <input type="text" placeholder="Логин" {...register("username")} />
+      <input type="text" placeholder="Пароль" {...register("password")} />
+      <button type="submit">Войти</button>
+    </form>
   );
 };
 export { LoginFeature };
--- src/widgets/loginWidget/ui/LoginWidget.tsx
+++ src/widgets/loginWidget/ui/LoginWidget.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 
 import { LoginFeature } from "@/features/loginFeature/ui";
+import s from "./LoginWidget.module.css";
 
 const LoginWidget: FC = () => {
   return (
-    <div>
+    <div className={s.container}>
       <LoginFeature />
     </div>
   );
